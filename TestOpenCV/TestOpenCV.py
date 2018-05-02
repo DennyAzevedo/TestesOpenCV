b3818d54619f02378bf0103208e1499944984798
Denny Azevedo <denny_af@yahoo.com.br>
2018-05-02 17:04:35 -0300

    Inicio dos Testes

--- TestOpenCV/TestOpenCV.py
+++ TestOpenCV/TestOpenCV.py
@@ -1,1 +1,26 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as mpl
 
+#Start load an image
+imLoad = cv2.imread("vencedor.png")
+
+#cv2.imshow("Window Name", imLoad)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
+#mpl.imshow(imLoad)
+#mpl.show()
+
+#cv2.imwrite("Output.png",imLoad)
+
+#Start to capture video
+cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc,20.0,(640,480))
+while True:
+	ret,frame = cap.read()
+	out.write(frame)
+	cv2.imshow("Video",frame)
+	if cv2.waitKey(1)&0xFF == ord('q'):
+		break
